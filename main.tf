6db6aaf2dea941c573b251e922adfa5c6b953ff5
kelseyy9 <kelsey.wilson998@gmail.com>
2023-09-01 15:28:50 -0500

    testing iam user credentials

--- main.tf
+++ main.tf
@@ -1,5 +1,12 @@
 provider "aws" {
-  region     = "us-east-1"
-  access_key = var.AWS_ACCESS_KEY
-  secret_key = var.AWS_SECRET_KEY
+  region     = "us-east-2"
+}
+
+resource "aws_instance" "example_server" {
+  ami           = "ami-0cf0e376c672104d6"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "iamtest"
+  }
 }
