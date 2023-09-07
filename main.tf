ef5cac7fe0fce905b8c732455feb218826e5ebcf
kelseyy9 <kelsey.wilson998@gmail.com>
2023-09-07 11:06:11 -0500

    edit main

--- main.tf
+++ main.tf
@@ -1,14 +1,3 @@
-provider "aws" {
-  region     = "us-east-2"
-  access_key = var.AWS_ACCESS_KEY
-  secret_key = var.AWS_SECRET_KEY
-}
+terraform {
 
-resource "aws_instance" "example_server" {
-  ami           = "ami-0cf0e376c672104d6"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "iamtest"
-  }
 }
